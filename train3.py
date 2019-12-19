8d62d893c73a1bd8b08164ccc2df2872434526f0
Filipe Zappe <filipezf@yahoo.com.br>
2019-12-19 16:40:15 -0300

    Update train3.py

--- train3.py
+++ train3.py
@@ -20,147 +20,219 @@ narratives (multi-statement procedural/declarative)
 #generic verbs: v1 v2 v3 v20 ?
 
 def train():
+    for i in range(500):
+        for p in pop:
+            train_a(p)
+        env.step()
+        
+        
+        
+#TODO: option to learn from input speech 
+#and reinforcement when speaks relevant text        
+
+
+def e_level(ag):
+    if ag.e < 5: return 'low'
+    if ag.e < 10: return 'mid'
+    return 'high'
+
+
+def age_level():
+    if ag.e < 20: return 'young'
+    if ag.e < 40: return 'adult'
+    return 'old'
     
-    step: e, nn[0,10]
-          age [ nn_age]
-          gender nn[gender]
-          
-      a e is N
-      a gender is N
-      a age is N
-      a is live / dead
+def setLang(ag, ev):
+    ag.hear(ev )
+    ag.setTxt( ev )
+    ag.desiredOut( speak ev ) 
+
+def train_a(ag):    
+    
+    # bias: hear x -> put x into txt buffer ?
+    # bias: extend from single entity to generic entity
+    
+        a = rA()
       
-      predict x -> x happens -> positive reinforce
+         # agent has mapping from value to axis
+    ag.setTxt( a 'be' 'energy' e_level(a.e) )
+    ag.setTxt( a 'be' 'age' age_level(a.age) )    
+    ag.setTxt( a 'be' 'gender' a.gender )  
+    ag.setTxt( a 'be' 'live' if a in pop else 'dead' )      
 
 
-nn[ag1][e] -> predict
-
-x:txt -> transform ->> action
-
-    rule( input x give -> txt: x 'give')  inner txt
-    rule( x give -> speak x 'give' )
+    v = give/hit
+    ag.setInput(x v y)
+   setLang(ag,  x v y )
     
-    rule( x give y -> speak x 'give' y )
-    rule( x hit y -> speak x 'hit' y )
-
-    rule( input x speak tx -> txt: x 'speak' txt)  inner txt
-    rule( x speak txt y -> speak x 'txt' y )
-
-
-    input : x v y / z v w      txt ->    x v y WHILE z v w
- pick svo1 svo2   txt svo1 before svo2
-  pick svo1 svo2   txt svo2 after svo1
-
-     x v y -> setTimeout( txt:  x v y -ed )
-     txt x v y will, ->  setTimeout( x v y, 0-5)
-     
-     txt x v y soon, ->  setTimeout( x v y, 0-5)
-      txt x v y later, ->  setTimeout( x v y, 10-20)
-
-     
-     input a vb avc avb  a v b 3 times
-     input avb    txt avb -ed
-     txt  avb
-
-because   <-> txt1 txt2  (causality) % 
-IF   <-> txt1 txt2  (causality) %
-
-
-    input a vo, b vo ->  a AND b vo
-          a v o  b v o c vo  -> AND a b c
-         a v o a v2 o -> a AND v v2 o
+    v = speak
+    ag.setInput(x v y)
+    setLang(ag,  x speak y txt)
+    
+    svo = pickEvent()
+    svo2 = pickEvent()
+    
+    w= WHILE
+    if svo.t < svo2.t: w = before
+    if svo.t > svo2.t: w = after    
+    setLang(ag,  svo w svo2)
+    
+    svo = ag.pickEvent()   
+    setLang(ag,  speak svo -ed ) 
+        
+    svo=createEvent()
+    dt = random.randint(0,10)
+    w = ''
+    if coin(0.5):
+        w = soon if dx < 5 else later        
+    setTimeout( svo , dt )
+    setLang(svo will w)
+    
+    
+    
+    ag.setInput([a v o, b v o ] )
+    setLang( (AND a b) v o )
+    
+    ag.setInput([a v o, b v o, cvo ] )
+    setLang( (AND a b c) v o )
+    
+    ag.setInput([a v o, a v2 o ] )
+    setLang( a (AND v v2) o )
          
-         a OR b will vo -> a vo  /// b v o
-         
-        a NOT vo -> b vo c vo
-        x v y ? NOT
-        x v y ? YES
+    setLang( (OR a b) v o )
+    x = a if coin(0.5) else b
+    setTimeout( ag.setInput( x v o)  )
 
+    #    a NOT vo -> b vo c vo
+    #    x v y ? NOT
+    #    x v y ? YES
+        
+        
+    svo = ag.pickEvent()
+    svo2 = ag.pickCause(svo)
+    if svo2 is not None:
+         setLang( svo2 because svo )
+         
+    svo = ag.generateEvent()
+    svo2 = ag.imagineFuture(svo)     
+    setLang( IF svo svo2 )
+
+
+    # questions
 
     # TODO
-    input I/b believe getBelief(b) -> toTxt
-    input I/b want getWant(b) -> toTxt
-    input I/b can getPossibleAction(b) -> toTxt
-    input I/b should getGoodeAction(b) -> toTxt
-
-
     
-    
-# mate
+    b = ag if coin(0.5) else rA()
+    b = rA()
+    setLang( b believe b.getBelief() )
+    setLang( b want  b.getWant() )
+    setLang( b can getPossibleAction() )
+    setLang( b should getGoodAction() )
+      
+     # ask / answer
 
-
-
-# parenting
-u += u_kin
-
-
-# reciprocal
-score[b] += help_hit b
-
-
-# ingroup / hierarchical
-ingroup[b]
-rank[x,b]
-
-# outgroup
-
-
-    s v o ?  
-    p ask q
-    q answer p
-
-    knowledge, uncertainty?
-        (Permit p, q ,txt) ==  (say p, q ( PERM txt)
+    #knowledge, uncertainty?
+    #    (Permit p, q ,txt) ==  (say p, q ( PERM txt)
         
+     
+    # order
+    setInput(b speak (c v1 !) )
+    setInput(c v1 )
+    setLang( b order (c v1), t=4 )
+    
+    setInput( d speak (d v2 !) , t=1)
+    setInput( e v3 , t=2)
+    setInput( d hit e, t=3)
+    
+    setLang( d order (e v2), t=4 )
+    
+
+     
+     # forbid
+     setInput (  b speak (c NOT v1!) )
+     setLang( b forbid c v1)
+     
+     
+     # member_of
+     setLang( c member_of g)
+     
+     
+     
+     # join
+     setTxt( c NOT member_of G )
+     setLang( c join G)
+     setTxt(c member_of G)
+     
+     #friend
+      setTxt( c e low)
+      setInput(b give c)
+      setLang( b friend c)
+      
+     #enemy / antonym
+      setTxt( b NOT friend c)
+      setTxt( c e low)
+      setInput( b give c)
+     setLang( b enemy c)
+      
+      
+     
+     # revenge
+     setTxt( a hurt b)
+     setTxt( b hurt a )
+      setLang( b revenge, a)
+
         
-    
-    (order p, q ,txt   -> q txt 
-     order p q txt       NOT q txt  -> p hit q
+     #betray
+     setTxt( a hurt b)
+     setTxt( b hurt a )
+     setLang( b revenge, a)
+      
+         ,(betray a, b)== (believe ,a ,(friend, b,a)) => hurt(b,a)
+         
+         
+     # lie
+     setInput( a h b)
+     setTxt( c believe a h b)
+     setInput( c speak f h d)
+     setInput( d speak a h b)
+     setLang( c lie )
+     setLang( d truth_tell )     
+       
+     # pretend      
+     # suppose      
+     # communism      
+     # free-will
+     # definiiton     
+     # abstract
      
-     p say NOT a x y <-> p deny a x y
+      'this sentence is false'
      
+     #self-referential 
+     # infinity of primes
+     # twin prime conjecture
+     # w-category
+     # non-Hausdoff manifold
      
-    ,(join ,p, g) ==  NOT (IN, p, g ) => (IN ,p ,g)
-    ,(separate, p, g)==  (IN, p, g ) => NOT(IN ,p ,g)
-    ,(friend ,p, q: p goal => q goal
-    ,(enemy ,p, q   p goal => not q goal  
-    ,(revenge, b,a) == (hurt,a,b) =>(hurt b a)
-    ,(betray a, b)== (believe ,a ,(friend, b,a)) => hurt(b,a)
-    ,(learn ,q, txt) ==  (NOT, know ,q ,txt => know ,t, txt) 
+     """justice freedom care holyness loyalty respect accidents, agency, agreement, alternatives, apologies,
+     arbitration, attempts, bias, blame, coercion, commensuration, conflict, constraints, conventions,
+     costs, crimes, culpability, culture, debt, deception, decisions, dependence, deterrents, distractions,
+     domination, duress, duty, escalation, excuses, exoneration, failures, fairness, false beliefs,
+     forgiveness, freedom, goals, goodness, identity, ignorance, impairment, impartiality, innocence, 
+     intervention, justifications, mental models,mercy, mistakes, moral rules, norms, paragons, passion, 
+     persons, plans, pReferences, prohibitions, punishment, recklessness, reparations, reputation,
+     retaliation, shame, side-effects, strategies, temptation, tort, trust, universals, utility, 
+     values, vengeance, virtues, and will"""
     
     
-    #,(know ,b, o) == (believe, b,o), o
+    setInput( b die -ed)
+    setInput(c speak ag ( b will v?) )
+    
+    ag.setInput((c speak  IF (ag die) (ag will v?) ))
+    #setInput( c speak 'If you die then you will ?')
     
     
-   ''' addDef( 'inform  p, q, txt',  '(believe p txt) => (say ,p , q txt)' )
-    ,(persuade, p ,q ,txt) == ((NOT (believe ,q, txt); say p ,q, txt), THEN, (believe q txt))
-
-    ,(suggest ,p, txt ==  (say p, (good txt))
-    ,(accept p, q txt) == (suggest p q txt, q say do txt)
-    ,(decline q, p ,txt)== (suggest p q txt, q say not txt)
-    (forbid p, q ,txt )==  (p say  bad q do txt)     
-    (lie ,q,p, txt) == (believe p txt);(  say ,q ,not txt) 
-    #pretend p txt:  p act if txt
-    #suppose p txt  : think p if txt'''
-   
-    # metaphor
-    #emotion
-    
-    p die
-    p will ?
-    
-    
-    
-    #spirituality
-    
-    #? x can svo ,x not visible, x good
-    
-
-    # narrative ??
-    # philosophy
-    # math
-
-
+    # understand/generate metaphors
+    # create narratives
 
 
 def self_play(N):
@@ -168,12 +240,3 @@ def self_play(N):
         #set random goal ?
         env.step()
    
-
-
-
-
-
-    
-    
-    
-    
